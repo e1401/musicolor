7a4ef8bf304b44245eea20129029419cba18dafa
Mateja Cimpersak <matejacimpersak@gmail.com>
2023-09-09 12:37:45 +0200

    add code for SearchBox component

--- src/pages/SearchBox.tsx
+++ src/pages/SearchBox.tsx
@@ -1,7 +1,29 @@
+import { Box, TextField, InputAdornment } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
 const SearchBox = () => {
-    return (
-        <div>"lorem ipsum"</div>
-    )
-}
+  return (
+    <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+      <div>
+        <TextField
+          label="Search"
+         variant="outlined"
+          defaultValue="Search for artist or album"
+          id="outlined"
+          sx={{ m: 1, width: "40ch" }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start"
+                  sx={{ p: "10px" }}
+                  aria-label="search">
+                  <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </div>
+    </Box>
+  );
+};
 
 export default SearchBox;
