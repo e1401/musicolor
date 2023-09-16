462c2f92630a65b6e020755778f1a4aac78a2893
Mateja Cimpersak <matejacimpersak@gmail.com>
2023-09-16 10:40:07 +0200

    add new  variables for cleaner code

--- src/pages/SearchBox.test.tsx
+++ src/pages/SearchBox.test.tsx
@@ -11,26 +11,24 @@ describe("SearchBox", () => {
     const input = screen.getByRole("textbox", { name: /search/i });
     expect(input).toBeInTheDocument();
   });
-  it.only("should have buttons enabled when user types", async () => {
+
+  it("should have buttons enabled when user types", async () => {
     const searchButton = screen.getByRole("button", { name: /search/i });
     const clearButton = screen.getByRole("button", { name: /clear/i });
     expect(searchButton).toBeDisabled();
     expect(clearButton).toBeDisabled();
     const textbox = screen.getByRole("textbox", { name: /search/i });
-    await userEvent.type(textbox, "test");
+    userEvent.type(textbox, "test");
 
     expect(searchButton).toBeEnabled();
     expect(clearButton).toBeEnabled();
   });
+
   it("should clear the search box when clear button is clicked", async () => {
-    await userEvent.type(
-      screen.getByRole("textbox", { name: /search/i }),
-      "test"
-    );
-    expect(screen.getByRole("textbox", { name: /search/i })).toHaveValue(
-      "test"
-    );
-    await userEvent.click(screen.getByRole("button", { name: /clear/i }));
+    const textbox = screen.getByRole("textbox", { name: /search/i });
+    userEvent.type(textbox, "test");
+    expect(textbox).toHaveValue("test");
+    userEvent.click(screen.getByRole("button", { name: /clear/i }));
     expect(screen.getByRole("textbox", { name: /search/i })).toHaveValue("");
   });
 });
