e4b243bb8b92af45bfa77d06d834181eab9a096c
Mateja Cimpersak <matejacimpersak@gmail.com>
2023-10-18 16:44:58 +0200

    add ResultCard with code for mapping trough mockData & rendering results (#12)
    * add ResultCard with code for mapping trough mockData & rendering results
    
    * add changes recommended in CR

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -1,93 +1,49 @@
-import { Box, Chip, Stack } from '@mui/material';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
+import { Box, Grid, Stack, Typography } from "@mui/material";
+import ResultCard from "../features/ResultCard";
+import { mockData } from "../__mocks__/mockData";
+import { ResultType } from "../types/result";
 
 const Results = () => {
-    return (
-        <Stack
-            flexDirection='column'
-            justifyContent='center'
-            alignItems='center'
-            height='100vh'
-            gap={2}
-            spacing={1}
-        >
-            <Paper elevation={3}>
-                <Card
-                    sx={{
-                        maxWidth: '244px',
-                    }}
-                >
-                    <CardMedia
-                        component='img'
-                        alt='artist cover'
-                        height='200px'
-                        image='https://is1-ssl.mzstatic.com/image/thumb/Music115/v4/5b/8d/47/5b8d47da-71ea-93ab-dffc-733f11332659/825646290703.jpg/100x100bb.jpg'
-                    />
-                    <Box sx={{ padding: '16px' }}>
-                        <CardContent
-                            sx={{
-                                display: 'flex',
-                                flexDirection: 'column',
-                                color: 'primary.main',
-                                fontSize: '14px',
-                                fontWeight: 'medium',
-                                padding: '0',
-                                paddingBottom: '0',
-                                '--pb-last': 'unset',
-                            }}
-                        >
-                            <Box
-                                sx={{
-                                    display: 'flex',
-                                    flexDirection: 'row',
-                                    justifyContent: 'space-between',
-                                }}
-                            >
-                                <Typography gutterBottom paddingRight={2}>
-                                    artistName
-                                </Typography>
-                                <Typography gutterBottom>
-                                    release date
-                                </Typography>
-                            </Box>
-
-                            <Typography
-                                gutterBottom
-                                variant='h6'
-                                color='text.primary'
-                                sx={{
-                                    fontSize: '20px',
-                                    fontWeight: 500,
-                                    lineHeight: '160%',
-                                    letterSpacing: '0.15px',
-                                }}
-                            >
-                                trackName
-                            </Typography>
-                            <Box
-                                sx={{
-                                    display: 'flex',
-                                    flexDirection: 'row',
-                                    justifyContent: 'space-between',
-                                }}
-                            >
-                                <Chip label='kind' size='medium' />
-                                <Chip
-                                    label='primaryGenreName'
-                                    size='medium'
-                                    variant='filled'
-                                />
-                            </Box>
-                        </CardContent>
-                    </Box>
-                </Card>
-            </Paper>
-        </Stack>
-    );
+  const results: ResultType[] = mockData;
+  return (
+    <Stack
+      flexDirection="row"
+      flexWrap="wrap"
+      justifyContent="flex-start"
+      alignItems="flex-start"
+      align-content="stretch"
+      height="100vh"
+    >
+      <Typography variant="h2" color="initial" paddingInlineStart={6}>
+        Results
+      </Typography>
+      <Grid
+        container
+        spacing={2}
+        gap={2}
+        justifyContent="center"
+        alignItems="center"
+        direction="row"
+      >
+        {results.map((result: ResultType, index) => {
+          return (
+            <Box key={index}>
+              <ResultCard
+                artistName={result.artistName}
+                trackName={result.trackName}
+                artworkUrl100={result.artworkUrl100
+                  .slice(1, -1)
+                  .replace("100x100", "500x500")}
+                kind={result.kind}
+                releaseDate={result.releaseDate.slice(0, 10)}
+                primaryGenreName={result.primaryGenreName}
+              />
+            </Box>
+          );
+        })}
+      </Grid>
+    </Stack>
+  );
 };
 
 export default Results;
--- src/pages/SearchBox.tsx
+++ src/pages/SearchBox.tsx
@@ -1,102 +1,104 @@
 import {
-    Stack,
-    Box,
-    TextField,
-    InputAdornment,
-    Button,
-    Link,
-} from '@mui/material';
-import { Link as RouterLink } from 'react-router-dom';
-import SearchIcon from '@mui/icons-material/Search';
-import { ChangeEvent, useState, KeyboardEvent } from 'react';
-import axios from 'axios';
-import { API_URL } from '../config/API_URL';
+  Stack,
+  Box,
+  TextField,
+  InputAdornment,
+  Button,
+  Link,
+} from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import SearchIcon from "@mui/icons-material/Search";
+import { ChangeEvent, useState, KeyboardEvent } from "react";
+import axios from "axios";
+import { API_URL } from "../config/API_URL";
 
 const SearchBox = () => {
-    const [value, setValue] = useState<string>('');
+  const [value, setValue] = useState<string>("");
 
-    const handleClear = () => {
-        setValue('');
-    };
+  const handleClear = () => {
+    setValue("");
+  };
 
-    const handleInputSearch = (e: ChangeEvent<HTMLInputElement>) => {
-        setValue(e.target.value);
-    };
+  const handleInputSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
 
-    const getSearchResults = async (searchValue: string) => {
-        const results = await axios
-            .get(API_URL + `?term=${searchValue}&limit=10`)
-            .then((res) => {
-                return res.data;
-            });
+  const getSearchResults = async (searchValue: string) => {
+    const results = await axios
+      .get(API_URL + `?term=${searchValue}&limit=10`)
+      .then((res) => {
+        return res.data;
+      });
+  };
 
-        console.log('Results are: ', results);
-    };
-
-    return (
-        <Stack
-            flexDirection='column'
-            justifyContent='center'
-            alignItems='center'
-            height='100vh'
-            gap={2}
+  return (
+    <Stack
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+      gap={2}
+    >
+      <Box width="40%" p={2}>
+        <Link
+          component={RouterLink}
+          to="/results"
+          paddingBottom={2}
+          display="block"
         >
-            <Box width='40%' p={2}>
-                <Link href='Results'>Results</Link>
-                <Link component={RouterLink} to='/results'>
-                    Results
-                </Link>
-                <TextField
-                    fullWidth
-                    id='input-search'
-                    label='Search'
-                    variant='outlined'
-                    placeholder='Search for artist or album'
-                    autoFocus
-                    type='text'
-                    value={value}
-                    onChange={handleInputSearch}
-                    onKeyDown={(e: KeyboardEvent<HTMLInputElement>) => {
-                        if (e.key === 'Enter') {
-                            getSearchResults(value);
-                        }
-                    }}
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position='start'>
-                                <SearchIcon />
-                            </InputAdornment>
-                        ),
-                    }}
-                />
-            </Box>
-            <Stack spacing={2} direction='row'>
-                <Button
-                    onClick={() => {
-                        getSearchResults(value);
-                    }}
-                    disabled={!value}
-                    variant='contained'
-                    color='primary'
-                    size='large'
-                    aria-label='search'
-                >
-                    Search
-                </Button>
+          Results
+        </Link>
+        <TextField
+          fullWidth
+          id="input-search"
+          label="Search"
+          variant="outlined"
+          placeholder="Search for artist or album"
+          autoFocus
+          type="text"
+          value={value}
+          onChange={handleInputSearch}
+          onKeyDown={(e: KeyboardEvent<HTMLInputElement>) => {
+            if (e.key === "Enter") {
+              getSearchResults(value);
+            }
+          }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Box>
+      <Stack spacing={2} direction="row">
+        <Button
+          onClick={() => {
+            getSearchResults(value);
+          }}
+          disabled={!value}
+          variant="contained"
+          color="primary"
+          size="large"
+          aria-label="search"
+        >
+          Search
+        </Button>
 
-                <Button
-                    disabled={!value}
-                    onClick={handleClear}
-                    variant='outlined'
-                    color='primary'
-                    size='large'
-                    aria-label='clear'
-                >
-                    Clear
-                </Button>
-            </Stack>
-        </Stack>
-    );
+        <Button
+          disabled={!value}
+          onClick={handleClear}
+          variant="outlined"
+          color="primary"
+          size="large"
+          aria-label="clear"
+        >
+          Clear
+        </Button>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default SearchBox;
