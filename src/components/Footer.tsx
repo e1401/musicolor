dc2becb01e55b0776ea02eb212cabced9499b67f
Mateja Cimpersak <matejacimpersak@gmail.com>
2024-01-03 21:18:00 +0100

    add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,24 @@
-import { Box, Typography } from '@mui/material';
-import { grey } from '@mui/material/colors';
+import { Box, Typography } from "@mui/material";
+import { grey } from "@mui/material/colors";
 
 const Footer = () => {
-    return (
-        <footer className='footer'>
-            <Box
-                display='flex'
-                flexDirection='column'
-                alignItems='center'
-                py={4}
-            >
-                <Typography variant='caption' color={grey[400]}>
-                    Made with ❤️ for colors and music
-                </Typography>
-            </Box>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        py={4}
+        bottom={0}
+        width={"100%"}
+        marginTop={"auto"}
+      >
+        <Typography variant="caption" color={grey[400]}>
+          Made with ❤️ for colors and music
+        </Typography>
+      </Box>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,34 @@
-import { AppBar, Box, Button, Toolbar } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import logo_img from '../images/logo_img.svg';
+import { AppBar, Box, Toolbar } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import logo_img from "../images/logo_img.svg";
 const Header = () => {
-    const navigate = useNavigate();
-    return (
-        <AppBar
-            position='static'
-            sx={{
-                boxShadow: 'none',
-                backgroundColor: 'transparent',
-                height: '64px',
-                mb: 3,
+  const navigate = useNavigate();
+  return (
+    <AppBar
+      position="static"
+      sx={{
+        boxShadow: "none",
+        backgroundColor: "transparent",
+        height: "64px",
+        mb: 3,
+      }}
+    >
+      <Toolbar>
+        <Box width={{ xs: "40%", md: "50%" }}>
+          <img
+            src={logo_img}
+            alt="Musicolor logo"
+            style={{
+              maxWidth: "100%",
+              height: "auto",
+              flex: "1",
             }}
-        >
-            <Toolbar>
-                <Box width={{ xs: '40%', md: '50%' }}>
-                    <img
-                        src={logo_img}
-                        alt='Musicolor logo'
-                        style={{
-                            maxWidth: '100%',
-                            height: 'auto',
-                            flex: '1',
-                        }}
-                        onClick={() => navigate('/')}
-                    />
-                </Box>
-            </Toolbar>
-        </AppBar>
-    );
+            onClick={() => navigate("/")}
+          />
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
