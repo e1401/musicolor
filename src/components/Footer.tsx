7005afad4788aa1ac50ab62fadc91c8d680969ef
Mateja Cimpersak <matejacimpersak@gmail.com>
2024-01-04 16:09:51 +0100

    add changes to the components and add single qoutes rule

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,26 @@
-import { Box, Typography } from '@mui/material';
+import { Box, CssBaseline, Typography } from '@mui/material';
 import { grey } from '@mui/material/colors';
 
 const Footer = () => {
-    return (
-        <footer className='footer'>
-            <Box
-                display='flex'
-                flexDirection='column'
-                alignItems='center'
-                py={4}
-            >
-                <Typography variant='caption' color={grey[400]}>
-                    Made with ❤️ for colors and music
-                </Typography>
-            </Box>
-        </footer>
-    );
+  return (
+    <footer className='footer'>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          minHeight: '100vh',
+        }}
+      >
+        <CssBaseline />
+        <Box py={4} marginTop={'auto'}>
+          <Typography variant='caption' color={grey[400]}>
+            Made with ❤️ for colors and music
+          </Typography>
+        </Box>
+      </Box>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,10 +1,10 @@
 import {
-    Stack,
-    Box,
-    TextField,
-    InputAdornment,
-    Button,
-    FormHelperText,
+  Stack,
+  Box,
+  TextField,
+  InputAdornment,
+  Button,
+  FormHelperText,
 } from '@mui/material';
 
 import SearchIcon from '@mui/icons-material/Search';
@@ -13,110 +13,112 @@ import axios from 'axios';
 import { API_URL } from '../config/API_URL';
 
 interface SearchBoxProps {
-    setSearchResults: React.Dispatch<React.SetStateAction<never[]>>;
+  setSearchResults: React.Dispatch<React.SetStateAction<never[]>>;
 }
 
 const SearchBox = ({ setSearchResults }: SearchBoxProps) => {
-    const [input, setInput] = useState<string>('');
-    const [helperText, setHelperText] = useState('');
+  const [input, setInput] = useState<string>('');
+  const [helperText, setHelperText] = useState('');
 
-    const handleClear = () => {
-        setInput('');
-    };
+  const handleClear = () => {
+    setInput('');
+  };
 
-    const handleInputSearch = (e: ChangeEvent<HTMLInputElement>) => {
-        setInput(e.target.value);
-    };
+  const handleInputSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
 
-    const getSearchResults = async (searchValue: string) => {
-        try {
-            const {
-                data: { results },
-            } = await axios.get(
-                API_URL + `?term=${searchValue}&entity=album&limit=10`
-            );
-            setSearchResults(results);
-        } catch (error) {
-            setHelperText('No results match criteria.');
-        }
-    };
+  const getSearchResults = async (searchValue: string) => {
+    try {
+      const {
+        data: { results },
+      } = await axios.get(
+        API_URL + `?term=${searchValue}&entity=album&limit=10`
+      );
+      setSearchResults(results);
+    } catch (error) {
+      setHelperText('No results match criteria.');
+    }
+  };
 
-    return (
-        <Stack
-            flexDirection='row'
-            flexWrap='wrap'
-            justifyContent='flex-start'
-            alignItems='center'
-            mt={5}
-            marginInlineStart={10}
+  return (
+    <Stack
+      flexDirection={'row'}
+      flexWrap='wrap'
+      justifyContent='flex-start'
+      alignItems='center'
+      mx={{ xs: 2, md: 4 }}
+      my={4}
+      gap={4}
+      width='100%'
+    >
+      <Box flexGrow={3}>
+        <TextField
+          aria-describedby='input-search'
+          fullWidth
+          id='input-search'
+          label='Search'
+          variant='outlined'
+          placeholder='Search for artist or album'
+          autoFocus
+          type='text'
+          value={input}
+          onChange={handleInputSearch}
+          onKeyDown={(e: KeyboardEvent<HTMLInputElement>) => {
+            if (e.key === 'Enter') {
+              getSearchResults(input);
+            }
+          }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position='start'>
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <FormHelperText
+          id='input-search'
+          sx={{
+            color: 'warning.main',
+            fontSize: '14px',
+            position: 'absolute',
+          }}
         >
-            <Box width='40%' p={2} flex='2 0 400px'>
-                <TextField
-                    aria-describedby='input-search'
-                    fullWidth
-                    id='input-search'
-                    label='Search'
-                    variant='outlined'
-                    placeholder='Search for artist or album'
-                    autoFocus
-                    type='text'
-                    value={input}
-                    onChange={handleInputSearch}
-                    onKeyDown={(e: KeyboardEvent<HTMLInputElement>) => {
-                        if (e.key === 'Enter') {
-                            getSearchResults(input);
-                        }
-                    }}
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position='start'>
-                                <SearchIcon />
-                            </InputAdornment>
-                        ),
-                    }}
-                />
-                <FormHelperText
-                    id='input-search'
-                    sx={{
-                        color: 'warning.main',
-                        fontSize: '14px',
-                        position: 'absolute',
-                    }}
-                >
-                    {helperText || ' '}
-                </FormHelperText>
-            </Box>
-            <Stack
-                spacing={2}
-                direction='row'
-                justifyContent='center'
-                flex='1 0 100px'
-            >
-                <Button
-                    onClick={() => {
-                        getSearchResults(input);
-                    }}
-                    disabled={!input}
-                    variant='contained'
-                    color='primary'
-                    size='large'
-                    aria-label='search'
-                >
-                    Search
-                </Button>
-                <Button
-                    disabled={!input}
-                    onClick={handleClear}
-                    variant='outlined'
-                    color='primary'
-                    size='large'
-                    aria-label='clear'
-                >
-                    Clear
-                </Button>
-            </Stack>
-        </Stack>
-    );
+          {helperText || ' '}
+        </FormHelperText>
+      </Box>
+      <Stack
+        spacing={2}
+        direction='row'
+        flexWrap={'nowrap'}
+        justifyContent='center'
+      >
+        <Button
+          onClick={() => {
+            getSearchResults(input);
+          }}
+          disabled={!input}
+          variant='contained'
+          color='primary'
+          size='large'
+          aria-label='search'
+        >
+          Search
+        </Button>
+        <Button
+          disabled={!input}
+          onClick={handleClear}
+          variant='outlined'
+          color='primary'
+          size='large'
+          aria-label='clear'
+        >
+          Clear
+        </Button>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default SearchBox;
