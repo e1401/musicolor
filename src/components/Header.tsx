98e042ca1634ecae0d8288fefdc0b8d42018363f
e1401 <hadrovic1@gmail.com>
2024-01-07 15:25:59 +0100

    Add dark mode switcher, some layout tweaks

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,67 @@
 import { AppBar, Box, Button, Toolbar } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 import { useNavigate } from 'react-router-dom';
+import { useDarkModeStore } from '../hooks/store';
 import logo_img from '../images/logo_img.svg';
+import logo_negative from '../images/logo_negative.svg';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { is } from 'date-fns/locale';
+
 const Header = () => {
+    const theme = useTheme();
+
+    const palette = theme.palette;
+
+    const { isDarkMode, toggleDarkMode } = useDarkModeStore();
+
     const navigate = useNavigate();
     return (
         <AppBar
             position='static'
             sx={{
                 boxShadow: 'none',
-                backgroundColor: 'transparent',
+                backgroundColor: isDarkMode
+                    ? palette.background.paper
+                    : 'transparent',
                 height: '64px',
                 mb: 3,
             }}
         >
             <Toolbar>
-                <Box width={{ xs: '40%', md: '50%' }}>
+                <Box
+                    display='flex'
+                    alignItems='center'
+                    justifyContent='center'
+                    sx={{
+                        cursor: 'pointer',
+                    }}
+                >
                     <img
-                        src={logo_img}
+                        src={isDarkMode ? logo_negative : logo_img}
                         alt='Musicolor logo'
                         style={{
-                            maxWidth: '100%',
+                            maxWidth: '80%',
                             height: 'auto',
                             flex: '1',
                         }}
                         onClick={() => navigate('/')}
                     />
                 </Box>
+                <Box sx={{ flexGrow: 1 }} />
+                <Button color='inherit' onClick={() => toggleDarkMode()}>
+                    {isDarkMode ? (
+                        <Brightness7Icon />
+                    ) : (
+                        <Brightness4Icon
+                            sx={{
+                                color: isDarkMode
+                                    ? palette.primary.main
+                                    : palette.primary.dark,
+                            }}
+                        />
+                    )}
+                </Button>
             </Toolbar>
         </AppBar>
     );
