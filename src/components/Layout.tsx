5193592822a8692e58f4f475be9654789425b8e2
Mateja Cimpersak <matejacimpersak@gmail.com>
2024-02-01 23:53:54 +0100

    add changes to layout.tsx file

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,22 @@
 import { Box } from '@mui/material';
 
 interface LayoutProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
-    return (
-        <Box minHeight={'calc(100vh - (2*64px))'} pb={1}>
-            {children}
-        </Box>
-    );
+  return (
+    <Box
+      sx={(theme) => ({
+        width: '100%',
+        [theme.breakpoints.up('lg')]: { width: '80%' },
+      })}
+      minHeight={'calc(100vh - (2*64px))'}
+      pb={1}
+    >
+      {children}
+    </Box>
+  );
 };
 
 export default Layout;
